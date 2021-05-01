702ceacd1f07c883dfcc9532e5dc069729ee9217
Kevin Che <cheyuany@usc.edu>
2021-05-01 13:51:56 -0700

    get-budget.php

---
+++ get-budget.php
@@ -0,0 +1,42 @@
+<?php
+require 'config.php';
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+$user_id = $data['user_id'];
+
+if (!isset($user_id) && trim($user_id) === '') {
+	error_respond(401, 'No user_id provided.');
+}
+
+
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+if ($mysqli->connect_errno) {
+	error_respond(401, $mysqli->error);
+}
+
+
+$sql = "SELECT budget FROM user_info WHERE id = '$user_id';";
+
+$result_budget = $mysqli->query($sql);
+if (!$result_budget) {
+	error_respond(401, $mysqli->error);
+}
+if ($result_budget->num_rows == 0) {
+	error_respond(401, 'No such User.');
+}
+
+
+$budget = $result_budget->fetch_assoc()['budget'];
+if ($budget === null) {
+	error_respond(401, 'Null Budget.');
+}
+
+
+$response = [
+	'status_code' => 200,
+	'message' => 'Success.',
+	'budget' => $budget
+];
+
+echo json_encode($response);
