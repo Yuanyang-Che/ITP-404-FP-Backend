c1e00012ef1c187d18f9aaaad38b57ae1884130e
Kevin Che <cheyuany@usc.edu>
2021-05-01 21:28:18 -0700

    get-all-debt

---
+++ get-all-debt.php
@@ -0,0 +1,50 @@
+<?php
+require 'config.php';
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+$user_id = 1;
+if (!isset($user_id) && trim($user_id) === '') {
+	error_respond(401, 'No user_id provided.');
+}
+
+$mysqli = get_mysqli();
+
+$user_select_result = check_user_id($mysqli, $user_id);
+$username = $user_select_result->fetch_assoc()['username'];
+
+$statement = $mysqli->prepare("
+	SELECT id, amount, u1.username AS payer_name, u2.username AS receiver_name
+	FROM debt_info
+		LEFT JOIN user_info u1 ON u1.id = debt_info.payer_id
+		LEFT JOIN user_info u2 ON u2.id = debt_info.receiver_id
+	WHERE payer_id = ?
+	   OR receiver_id = ?;");
+$statement->bind_param('ii', $user_id, $user_id);
+$statement->execute();
+$debt_select_result = $statement->get_result();
+if (!$debt_select_result) {
+	error_respond(401, $mysqli->error);
+}
+if ($debt_select_result->num_rows != 0) {
+	error_respond(500, 'No Debt. ');
+}
+
+
+$debts = [];
+foreach ($debt_select_result as $row) {
+	$name = $row['payer_name'] != $username ? $row['payer_name'] : $row['receiver_name'];
+	$debt = [
+		'id' => $row['id'],
+		'amount' => $row['amount'],
+		'username' => $name,
+	];
+	array_push($debts, $debt);
+}
+
+$response = [
+	'status_code' => 200,
+	'debts' => $debts,
+];
+
+echo json_encode($response);
