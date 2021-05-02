ad2c4fee733a1984a41d424060b03c7bc79d70b0
Kevin Che <cheyuany@usc.edu>
2021-05-01 20:42:15 -0700

    remove-bill

--- remove-bill.php
+++ remove-bill.php
@@ -9,16 +9,28 @@ if (!isset($bill_id) && trim($bill_id) === '') {
 	error_respond(401, 'No bill_id provided.');
 }
 
+
 $mysqli = get_mysqli();
 
 $bill_select_result = check_bill_id($mysqli, $bill_id);
 
 $bill = $bill_select_result->fetch_assoc();
 
+
+$statement = $mysqli->prepare('
+	DELETE FROM bill_info WHERE id = ?;');
+$statement->bind_param('i', $bill_id);
+$bill_delete_result = $statement->execute();
+if (!$bill_delete_result) {
+	error_respond(401, $mysqli->error);
+}
+if ($statement->affected_rows != 1) {
+	error_respond(500, 'Delete Bill Failed. ');
+}
+
 $response = [
 	'status_code' => 200,
-	'message' => 'Get Bill Success',
-	'bill' => $bill
+	'message' => 'Remove Bill Success'
 ];
 
 echo json_encode($response);
