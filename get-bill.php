48d030b45fda1cfaffcc1bd4c4b3e2d8d8f6fea5
Kevin Che <cheyuany@usc.edu>
2021-05-01 20:25:52 -0700

    get-bill

--- get-bill.php
+++ get-bill.php
@@ -3,11 +3,22 @@ require 'config.php';
 
 $data = json_decode(file_get_contents('php://input'), true);
 
-$bill_id = $data['bill_id'];
+//$bill_id = $data['bill_id'];
+$bill_id = 1;
 if (!isset($bill_id) && trim($bill_id) === '') {
 	error_respond(401, 'No bill_id provided.');
 }
 
 $mysqli = get_mysqli();
 
-$bill_select_result = check_bill_id($bill_id);
+$bill_select_result = check_bill_id($mysqli, $bill_id);
+
+$bill = $bill_select_result->fetch_assoc();
+
+$response = [
+	'status_code' => 200,
+	'message' => 'Get Bill Success',
+	'bill' => $bill
+];
+
+echo json_encode($response);
