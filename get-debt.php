6cb5eee52b4c541d2d6cc1885e57e14c7971af08
Kevin Che <cheyuany@usc.edu>
2021-12-02 19:49:25 -0800

    Create get-debt.php

---
+++ get-debt.php
@@ -0,0 +1,44 @@
+<?php
+require 'config.php';
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+$debt_id = $data['debt_id'];
+
+if (!isset($debt_id) && trim($debt_id) === '') {
+	error_respond(401, 'No debt_id provided.');
+}
+
+$mysqli = get_mysqli();
+
+
+$statement = $mysqli->prepare("
+	SELECT receiver_id, amount
+	FROM debt_info
+	WHERE id = ?;");
+$statement->bind_param('i', $debt_id);
+$statement->execute();
+$debt_select_result = $statement->get_result();
+if (!$debt_select_result) {
+	error_respond(401, $mysqli->error);
+}
+if ($debt_select_result->num_rows === 0) {
+	error_respond(500, 'No Such Debt. ');
+}
+
+$debts = [];
+foreach ($debt_select_result as $row) {
+	$debt = [
+		'id' => $row['id'],
+		'amount' => $row['amount'],
+		'receiver_id' => $row['username']
+	];
+	array_push($debts, $debt);
+}
+
+$response = [
+	'status_code' => 200,
+	'debts' => $debts,
+];
+
+echo json_encode($response);
